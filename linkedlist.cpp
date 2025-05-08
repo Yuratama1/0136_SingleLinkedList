ddc444d571b9c05e23513d25f03a416b2232f163
Yuratama1 <yuratama661@gmail.com>
2025-05-08 15:31:00 +0700

    menyelesaikan prosedurnya

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -43,7 +43,14 @@ class List{
         while ((current != NULL) && (nim >= current->noMhs))
         {
             if(nim == current->noMhs){
-                
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
             }
+            previous = current;
+            current = current->next;
         }
+        nodeBaru->next = current;
+        previous->next - nodeBaru;
+
+        
 };
