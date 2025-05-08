134f4ac5b21edf285a1988b3026a2e354510da28
Yuratama1 <yuratama661@gmail.com>
2025-05-08 15:42:00 +0700

    memyelesaikan bool denode

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -67,5 +67,14 @@ class List{
             return (*current != NULL);
         }
 
-        
+        bool delNode(int nim){
+            Node *current, *previous;
+            if (!search(nim, &previous, &current))
+                return false;
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->next;
+            delete current;
+        }
 };
