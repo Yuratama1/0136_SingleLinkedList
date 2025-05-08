8874d456008702117887adb78e2d5ef280dae239
Yuratama1 <yuratama661@gmail.com>
2025-05-08 15:15:02 +0700

    membuat class list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 class Node{
-    
-}
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class List{
+    Node *START;
+
+    public:
+        List(){
+            START = NULL;
+        }
+        
+};
